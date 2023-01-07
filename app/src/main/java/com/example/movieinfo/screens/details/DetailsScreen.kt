5091b0911b6c3f4fdb88e0513177851971cf3cf8
nqobani <nqobanizulu15@gmail.com>
2023-01-07 20:13:35 +0200

    Cleanup

--- app/src/main/java/com/example/movieinfo/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieinfo/screens/details/DetailsScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.ExperimentalUnitApi
